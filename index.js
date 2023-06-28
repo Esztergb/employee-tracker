373e286ef2beb53b22524158b1691cf62fa614a8
Eszter Barbuscia <gubiscom@yahoo.com>
2023-06-27 23:03:59 -0700

    db connection

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+//dependencies
+const inquirer = require('inquirer');
+const mysql = require('mysql2/promise');
+const cTable = require("console.table");
+
+//connect to database
+require("dotenv").config();
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: process.env.DB_USER,
+    //MySQL password here
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  }
+  //   console.log(`Connected to the employees_db database.`)
+);
+
+
+
+
 // Give the user a pleasant welcome message.
 console.log("***********************************");
 console.log("*                                 *");
@@ -6,10 +30,82 @@ console.log("*                                 *");
 console.log("***********************************");
 console.log("")
 
-const inquirer = require('inquirer');
-const mysql = require('mysql2');
-const prompts = require ('./utils/prompts');
-const connection = require('./config/connection');
-// const consoleTable = require("console.table");
 
- 
+
+const connection = require('./config/connection');
+require('dotenv').config();
+
+ const init = function () {
+   inquirer
+     .prompt({
+       type: "list",
+       name: "menu",
+       message: "What would you like to do?",
+       choices: [
+         "View All Employees",
+         "View All Departments",
+         "View All Roles",
+         "Add Employee",
+         "Add Department",
+         "Add Role",
+         "Remove Employee",
+         "Update Employee Role",
+         "Exit",
+       ],
+     })
+     .then(function (answer) {
+       switch (answer.menu) {
+         case "View All Employees":
+           viewEmployees()
+           .then ((queryResults)=>{ 
+            console.table(queryResults)
+            init()
+            })
+           break;
+
+         case "View All Departments":
+           viewDepartments()
+           .then((queryResults) => {
+             console.table(queryResults);
+             init();
+           });
+           break;
+
+         case "View All Roles":
+           viewRoles()
+           .then((queryResults) => {
+             console.table(queryResults);
+             init();
+           });
+           break;
+
+         case "Add Employee":
+           addEmployee();
+           break;
+
+         case "Add Department":
+           addDepartment();
+           break;
+
+         case "Add Role":
+           addRole();
+           break;
+
+         case "Update Employee Role":
+           updateRole();
+           break;
+
+         case "Exit":
+           connection.end();
+           break;
+         default:
+           break;
+       }
+     });
+ }
+
+ init();
+
+
+
+
